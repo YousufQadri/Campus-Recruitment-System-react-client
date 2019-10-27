29654c361219bd19d0d7735e696fe90e474ce069
YousufQadri <s.yousuf@live.com>
2019-10-27 21:09:41 +0500

    Add: Simple header component

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>Campus Recruitment System</h1>
+      </div>
+      <div>
+        <ul>
+          <li>Sign Up</li>
+          <li>Login</li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
