5195d0dfbec0fa8be9f800e558b17c9b5f660975
YousufQadri <s.yousuf@live.com>
2019-10-28 20:47:39 +0500

    Add: Header/Nav component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,17 +2,26 @@ import React, { Component } from "react";
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <h1>Campus Recruitment System</h1>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <h1 className="navbar-brand">Campus Recruitment System</h1>
+        <form className="nav navbar-nav navbar-right form-inline my-2 my-lg-0">
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
+            Sign Up
+          </button>
+
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
       </div>
-      <div>
-        <ul>
-          <li>Sign Up</li>
-          <li>Login</li>
-        </ul>
-      </div>
-    </header>
+    </nav>
   );
 };
 
