df65c6edb550c2ed7e9c67123e4c63f9fe57ca26
YousufQadri <s.yousuf@live.com>
2019-10-29 00:49:58 +0500

    Function: Dynamic header for logged in and out user

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import { getJWT } from "../helpers/jwt";
 
-const Header = () => {
+const token = getJWT();
+
+const Header = props => {
+  const logUserOut = () => {
+    localStorage.removeItem("jwt");
+    // console.log(props);
+
+    props.history.push("/");
+  };
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         <h1 className="navbar-brand">Campus Recruitment System</h1>
         <form className="nav navbar-nav navbar-right form-inline my-2 my-lg-0">
-          <Link to="/register" className="btn btn-outline-success my-2 my-sm-0">
-            {" "}
-            Sign Up
-          </Link>
-          <Link to="/login" className="btn btn-outline-success my-2 my-sm-0">
-            {" "}
-            Log In
-          </Link>
+          {!token ? (
+            <div>
+              <Link
+                to="/register"
+                className="btn btn-outline-success my-2 my-sm-0 mr-2"
+              >
+                {" "}
+                Sign Up
+              </Link>
+              <Link
+                to="/login"
+                className="btn btn-outline-success my-2 my-sm-0"
+              >
+                {" "}
+                Log In
+              </Link>
+            </div>
+          ) : (
+            <Link
+              to="/"
+              className="btn btn-outline-success my-2 my-sm-0"
+              onClick={() => logUserOut()}
+            >
+              {" "}
+              Log out
+            </Link>
+          )}
         </form>
       </div>
     </nav>
   );
 };
 
-export default Header;
+export default withRouter(Header);
