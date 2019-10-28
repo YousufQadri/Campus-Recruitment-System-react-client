27d70cda12681c140f9c9c423112d75d8cbdfe21
YousufQadri <s.yousuf@live.com>
2019-10-28 22:41:19 +0500

    Implement: Login/Register route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header";
-import SignUp from "./Components/SignUp";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
+import Home from "./screens/Home";
+import AuthComponent from "./Components/AuthComponent";
+import Protected from "./Components/Protected";
 
 class App extends React.Component {
   state = {};
@@ -13,9 +18,17 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <SignUp />
-        <header className="App-header">Working</header>
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/register" exact component={Register} />
+            <Route path="/login" exact component={Login} />
+            <AuthComponent>
+              <Route path="/protected" exact component={Protected} />
+            </AuthComponent>
+          </Switch>
+        </Router>
       </div>
     );
   }
