3ce71d3f3ce49984e593332fbce6ac66b08b2715
YousufQadri <s.yousuf@live.com>
2019-10-30 23:51:19 +0500

    Add: Student and company login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Alert from "./Alert";
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Row,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  Alert
+} from "reactstrap";
+// import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+// import classnames from "classnames";
+// import Alert from "./Alert";
 
 class Login extends Component {
   state = {
     email: "",
     password: "",
     message: "",
-    flag: false
+    flag: false,
+    activeTab: 1
   };
 
   componentDidUpdate() {
@@ -20,28 +37,55 @@ class Login extends Component {
     });
   };
 
-  formSubmit = e => {
+  studentFormSubmit = e => {
     e.preventDefault();
     let { email, password } = this.state;
 
     if (email && password) {
       axios
-        .post("http://localhost:5000/api/v1/user/login", {
+        .post("http://localhost:5000/api/v1/student/login", {
           email,
-          password,
-          type: "Student"
+          password
         })
         .then(res => {
           console.log(res.data);
+          this.setState({ message: res.data.message, flag: true });
           localStorage.setItem("jwt", res.data.token);
-          this.props.history.push("/dashboard");
-          // this.setState({ message: res.data.message, flag: true });
+          setTimeout(() => {
+            this.props.history.push("/dashboard");
+          }, 2000);
+
+          // console.log(res.data);
+        })
+        .catch(error => {
+          this.setState({ message: error.response.data.message, flag: false });
+          console.log("Error: ", error.response.data.message);
+        });
+    } else {
+      console.log("Fill all fields");
+    }
+  };
+  companyFormSubmit = e => {
+    e.preventDefault();
+    let { email, password } = this.state;
+
+    if (email && password) {
+      axios
+        .post("http://localhost:5000/api/v1/company/login", {
+          email,
+          password
+        })
+        .then(res => {
+          console.log(res.data);
+          // localStorage.setItem("jwt", res.data.token);
+          // this.props.history.push("/dashboard");
+          this.setState({ message: res.data.message, flag: true });
 
           // localStorage.setItem("token", res.data.token);
           // console.log(res.data);
         })
         .catch(error => {
-          // this.setState({ message: error.response.data.message, flag: false });
+          this.setState({ message: error.response.data.message, flag: false });
           console.log("Error: ", error.response.data.message);
         });
     } else {
@@ -49,37 +93,114 @@ class Login extends Component {
     }
   };
 
+  toggle = tab => {
+    if (this.state.activeTab !== tab)
+      this.setState({ activeTab: tab, email: "", password: "" });
+  };
+
   render() {
     return (
-      <div>
-        <h1>Log in to your account</h1>
+      <div className="Login">
+        <h1 className="mb-5">Log in to your account</h1>
         {this.state.message ? (
-          <Alert
-            message={this.state.message}
-            color={this.state.flag ? "green" : "red"}
-          />
+          <Alert color={this.state.flag ? "success" : "danger"}>
+            {this.state.message}
+          </Alert>
         ) : null}
-        <form onSubmit={this.formSubmit}>
-          <label>Email</label>
-          <input
-            type="email"
-            placeholder="Email"
-            onChange={this.onChange}
-            name="email"
-            value={this.state.email}
-            required
-          />
-          <label>Password</label>
-          <input
-            type="password"
-            placeholder="Password"
-            onChange={this.onChange}
-            name="password"
-            value={this.state.password}
-            required
-          />
-          <button type="submit">Submit</button>
-        </form>
+        <Nav tabs fill className="mb-3">
+          <NavItem>
+            <NavLink
+              onClick={() => {
+                this.toggle("1");
+              }}
+            >
+              Student
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              onClick={() => {
+                this.toggle("2");
+              }}
+            >
+              Company
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="1">
+            <Form onSubmit={e => this.studentFormSubmit(e)}>
+              <h4>Student Login</h4>
+              <FormGroup>
+                <Label for="email">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  required
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="pass">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  placeholder="Enter your password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  required
+                />
+              </FormGroup>
+              <Button
+                type="submit"
+                color="primary"
+                block
+                onClick={this.studentFormSubmit}
+              >
+                Submit
+              </Button>
+            </Form>
+          </TabPane>
+          <TabPane tabId="2">
+            <Form onSubmit={this.companyFormSubmit}>
+              <h4>Company Login</h4>
+              <FormGroup>
+                <Label for="email">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter your email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  required
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="pass">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  id="pass"
+                  placeholder="Enter your password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  required
+                />
+              </FormGroup>
+              <Button
+                type="submit"
+                color="primary"
+                block
+                onClick={this.companyFormSubmit}
+              >
+                Submit
+              </Button>
+            </Form>
+          </TabPane>
+        </TabContent>
       </div>
     );
   }
